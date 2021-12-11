6538dcd91cc85acf8bc05b6d8a8a288676c39071
Hackerprod <hackerprodlive@gmail.com>
2021-12-11 08:57:08 -0500

    Update

--- [SKYNET] DumpAnalyzer/Controls/SKYNET_Textbox.cs
+++ [SKYNET] DumpAnalyzer/Controls/SKYNET_Textbox.cs
@@ -8,45 +8,89 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SKYNET
+namespace SKYNET.Controls
 {
-    public partial class SKYNET_Textbox : UserControl
+    public partial class SKYNET_TextBox : UserControl
     {
-        private string _text;
-        private bool _IsPassword;
-
-        [Category("SKYNET")]
-        public event EventHandler OnReturnPressed;
-
         [Category("SKYNET")]
         public event EventHandler OnLogoClicked;
 
         [Category("SKYNET")]
-        public Color Control_BorderColor { get; set; }
+        public EventHandler OnReturnPressed { get; internal set; }
+
+        private bool isPassword;
+
+        public SKYNET_TextBox()
+        {
+            InitializeComponent();
+            textBox.BackColor = BackColor;
+
+            textBox.GotFocus += TextBox_GotFocus;
+            textBox.LostFocus += TextBox_LostFocus;
+            textBox.KeyDown += TextBox_KeyDown;
+            textBox.KeyUp += TextBox_KeyUp;
+            textBox.KeyPress += TextBox_KeyPress;
+            textBox.TextChanged += TextBox_TextChanged;
+
+            BackColor = Color;
+        }
 
         [Category("SKYNET")]
-        public Color Control_BackColor
+        public override string Text
         {
-            get
-            {
-                return BackColor;
-            }
+            get { return textBox.Text; }
             set
             {
-                BackColor = value;
-                ChangeColors(false);
+                textBox.Text = value;
             }
         }
-        [Category("SKYNET")]
-        public string Empty_Text
+
+        public override Font Font { get => textBox.Font; set => textBox.Font = value; }
+        protected override void OnTextChanged(EventArgs e)
         {
-            get
+            base.OnTextChanged(e);
+        }
+
+        [Category("SKYNET")]
+        public override Color BackColor
+        {
+            get { return backColor; }
+            set
             {
-                return lblSearch.Text;
+                backColor = value;
+                panel1.BackColor = value;
+                panel2.BackColor = value;
+                panel3.BackColor = value;
+                panel4.BackColor = value;
+                P_Container.BackColor = value;
+                textBox.BackColor = backColor;
+                textBox.Refresh();
+            }
+        }
+        private Color backColor;
+        [Category("SKYNET")]
+        public Color Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+            }
+        }
+        private Color color;
+
+
+        [Category("SKYNET")]
+        public override Color ForeColor
+        {
+            get 
+            { 
+                return textBox.ForeColor; 
             }
             set
             {
-                lblSearch.Text = value;
+                textBox.ForeColor = value;
+                base.ForeColor = value;
             }
         }
         [Category("SKYNET")]
@@ -84,208 +128,89 @@ namespace SKYNET
         }
 
         [Category("SKYNET")]
-        public Color ActivatedBackColor { get; set; }
+        public Color ActivatedBackColor
+        {
+            get { return focused_BackColor; }
+            set
+            {
+                focused_BackColor = value;
+            }
+        }
+        private Color focused_BackColor;
 
-
-
+        [Category("SKYNET")]
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                Container.BackColor = value;
+            }
+        }
         [Category("SKYNET")]
         public bool IsPassword
         {
-            get { return _IsPassword; }
+            get { return isPassword; }
             set
             {
-                _IsPassword = value;
+                isPassword = value;
                 if (value)
                 {
+                    textBox.PasswordChar = '*';
                     textBox.UseSystemPasswordChar = true;
                 }
                 else
-                {
                     textBox.UseSystemPasswordChar = false;
-                }
-            }
-        }
-        [Category("SKYNET")]
-        public new string Text
-        {
-            get { return _text; }
-            set
-            {
-                _text = value;
-                textBox.Text = _text;
-            }
-        }
-        public SKYNET_Textbox()
-        {
-            InitializeComponent();
-            textBox.Text = Text;
-            textBox.KeyDown += TextBox_KeyDown;
-            textBox.KeyPress += TextBox_KeyPress;
-            textBox.KeyUp += TextBox_KeyUp;
-            textBox.PreviewKeyDown += TextBox_PreviewKeyDown;
-
-            if (Control_BackColor == null)
-            {
-                Control_BackColor = BackColor;
-            }
-            if (ActivatedBackColor == null)
-            {
-                ActivatedBackColor = BackColor;
-            }
-            if (string.IsNullOrEmpty(textBox.Text))
-            {
-                lblSearch.Text = Empty_Text;
             }
         }
 
-        private void TexBox_TextChanged(object sender, EventArgs e)
-        {
-            textBox.ForeColor = ForeColor;
-            lblSearch.ForeColor = this.ForeColor;
 
-            if (string.IsNullOrEmpty(textBox.Text))
-            {
-                lblSearch.Visible = true;
-            }
-            else
-            {
-                lblSearch.Visible = false;
-            }
-            base.OnTextChanged(e);
-        }
-        private void TexBox_Enter(object sender, EventArgs e)
-        {
-            BackColor = Control_BorderColor;
-            ChangeColors(true);
-        }
+        private Color borderColor;
 
-        private void ChangeColors(bool ChangeColors)
-        {
-            if (ChangeColors)
-            {
-                SetBackColor(this, ActivatedBackColor);
-                textBox.BackColor = ActivatedBackColor;
-                lblSearch.BackColor = ActivatedBackColor;
-                panel1.BackColor = ActivatedBackColor;
-                panel2.BackColor = ActivatedBackColor;
-                panel3.BackColor = ActivatedBackColor;
-                panel4.BackColor = ActivatedBackColor;
-                panel5.BackColor = ActivatedBackColor;
-                logo_box.BackColor = ActivatedBackColor;
-
-            }
-            else
-            {
-                SetBackColor(this, Control_BackColor);
-                textBox.BackColor = Control_BackColor;
-                lblSearch.BackColor = Control_BackColor;
-                panel1.BackColor = Control_BackColor;
-                panel2.BackColor = Control_BackColor;
-                panel3.BackColor = Control_BackColor;
-                panel4.BackColor = Control_BackColor;
-                panel5.BackColor = Control_BackColor;
-                logo_box.BackColor = Control_BackColor;
-
-            }
-        }
-
-        private void TextBox_Leave(object sender, EventArgs e)
-        {
-            BackColor = Control_BackColor;
-            ChangeColors(false);
-        }
-
-        private void LblSearch_Click(object sender, EventArgs e)
+        private void OnMouseClick(object sender, MouseEventArgs e)
         {
             textBox.Focus();
         }
 
-
-
-        private void SearchTextBox_Load(object sender, EventArgs e)
+        private void LogoClick(object sender, MouseEventArgs e)
         {
-            lblSearch.BackColor = this.Control_BackColor;
-            textBox.BackColor = this.Control_BackColor;
-            panel1.BackColor = this.Control_BackColor;
-            panel2.BackColor = this.Control_BackColor;
-            panel3.BackColor = this.Control_BackColor;
-            panel4.BackColor = this.Control_BackColor;
-            panel5.BackColor = this.Control_BackColor;
-            logo_box.BackColor = this.Control_BackColor;
+            OnLogoClicked?.Invoke(this, e);
         }
-        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+
+        private void TextBox_GotFocus(object sender, EventArgs e)
         {
-            Text = textBox.Text;
-            base.OnKeyDown(e);
+            BackColor = ActivatedBackColor;
+            base.OnGotFocus(e);
         }
-        private void TextBox_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        private void TextBox_LostFocus(object sender, EventArgs e)
         {
-            Text = textBox.Text;
-            base.OnPreviewKeyDown(e);
+            BackColor = Color;
+            base.OnLostFocus(e);
+        }
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
         }
 
         private void TextBox_KeyUp(object sender, KeyEventArgs e)
         {
-            Text = textBox.Text;
             base.OnKeyUp(e);
-            if (e.KeyData == Keys.Return)
-            {
-                OnReturnPressed?.Invoke(this, e);
-            }
         }
 
-        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
-            Text = textBox.Text;
-            base.OnKeyPress(e);
+            base.OnKeyDown(e);
+        }
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            base.OnTextChanged(e);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            BackColor = Color;
+            base.OnPaint(e);
         }
 
-        private void LoginBox_ForeColorChanged(object sender, EventArgs e)
-        {
-            textBox.ForeColor = ForeColor;
-        }
-
-        private void LoginBox_FontChanged(object sender, EventArgs e)
-        {
-            textBox.Font = Font;
-        }
-        private void SetBackColor(Control Parent, Color color)
-        {
-            foreach (var item in Parent.Controls)
-            {
-                Control control = (Control)item;
-                InvokeBackColor(control, color);
-
-                if (control.Controls.Count > 0)
-                {
-                    SetBackColor(control, color);
-                }
-            }
-        }
-        private void InvokeBackColor(Control control, Color color)
-        {
-            if (control.InvokeRequired)
-            {
-                control.Invoke(new Action(() =>
-                {
-                    control.BackColor = color;
-                }));
-            }
-            else
-            {
-                control.BackColor = color;
-            }
-        }
-
-        private void Logo_box_MouseClick(object sender, MouseEventArgs e)
-        {
-            base.OnMouseClick(e);
-            OnLogoClicked.Invoke(this, e);
-        }
-
-        private void Control_MouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            base.OnMouseDoubleClick(e);
-        }
     }
 }
